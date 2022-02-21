3f249a4cbdb08f29a8978836b00034d9b5d48f55
Morteza Damghani <mortezadamghani77@gmail.com>
2022-02-21 00:19:24 -0800

    Naive Bayes part is almost donw

--- NaiveBayes/Unigram/UnigramOfflineTest.py
+++ NaiveBayes/Unigram/UnigramOfflineTest.py
@@ -94,32 +94,6 @@ def dictionary_loader():
         negative_dictionary[word] = count
         total_number_of_negative_words += count
 
-    # Loading neutral dictionary
-    # while True:
-    #     line = neutral_dictionary_file.readline()
-    #     if line == "":
-    #         break
-    #     index = line.find(" ")
-    #     line_list = list(line)
-    #     i = 0
-    #     word = ""
-    #     while i < index:
-    #         word += line_list[i]
-    #         i += 1
-    #     i = 1
-    #     while i <= 30:
-    #         line_list.pop(0)
-    #         i += 1
-    #     count = ""
-    #     i = 0
-    #     while i < len(line_list):
-    #         count += line_list[i]
-    #         i += 1
-    #     count = int(count)
-    #     neutral_dictionary[word] = count
-    #     total_number_of_neutral_words += count
-
-
     print("The length of positive tweets dictionary is: " + str(len(positive_dictionary)))
     print("Total number of words in positive words dictionary is: " + str(total_number_of_positive_words))
     print("The length of negative tweets dictionary is: " + str(len(negative_dictionary)))
@@ -136,12 +110,12 @@ positive_comments_dictionary, negative_comments_dictionary, total_number_of_posi
 # Unigram Naive Bayes method implementation
 # Negative comments test implementation
 print("Processing test files...")
-# t = total_number_of_positive_dictionary_words + total_number_of_negative_dictionary_words + total_number_of_neutral_dictionary_words
 negative_test_comments_file_address = "E://MortezaDamghaniNouri//Computer Engineering//Semesters//9//Computer Engineering Final Project//Final Decision Files//Train Dataset for Twitter//Dataset//Complete Dataset//Test//negative_test_tweets.txt"
 negative_test_comments_file = open(negative_test_comments_file_address, "rt")
 negative_result_file = open("negative_result.txt", "wt")
 comments_counter = 0
 true_categorization = 0
+lower_than_zero_counter = 0
 while True:
     comment = negative_test_comments_file.readline().lower()
     if comment == "":
@@ -150,8 +124,6 @@ while True:
 
     comment = comment_smoother(comment)
     words_list = comment.split(" ")
-    # negative_result = 0
-    # positive_result = 0
     final_result = 0
     for word in words_list:
         if word in positive_comments_dictionary:
@@ -166,12 +138,11 @@ while True:
 
         final_result += math.log10((positive_numerator / total_number_of_positive_dictionary_words) / (negative_numerator / total_number_of_negative_dictionary_words))
 
-
-
-
-
-
     negative_result_file.write(str(round(final_result, 2)) + "\n")
+    if final_result <= -0.1:
+        true_categorization += 1
+    if final_result < 0:
+        lower_than_zero_counter += 1
     comments_counter += 1
 
 negative_comments_precision = round((true_categorization / comments_counter) * 100, 2)
@@ -186,15 +157,13 @@ positive_test_comments_file = open(positive_test_comments_file_address, "rt")
 positive_result_file = open("positive_result.txt", "wt")
 comments_counter = 0
 true_categorization = 0
+more_than_zero_counter = 0
 while True:
     comment = positive_test_comments_file.readline()
     if comment == "":
         break
     comment = comment_smoother(comment)
     words_list = comment.split(" ")
-    # negative_result = 0
-    # positive_result = 0
-    # neutral_result = 0
     final_result = 0
     for word in words_list:
         if word in positive_comments_dictionary:
@@ -211,6 +180,11 @@ while True:
 
 
     positive_result_file.write(str(round(final_result, 2)) + "\n")
+
+    if final_result >= 0.1:
+        true_categorization += 1
+    if final_result > 0:
+        more_than_zero_counter += 1
     comments_counter += 1
 
 positive_comments_precision = round((true_categorization / comments_counter) * 100, 2)
@@ -230,9 +204,6 @@ while True:
         break
     comment = comment_smoother(comment)
     words_list = comment.split(" ")
-    # negative_result = 0
-    # positive_result = 0
-    # neutral_result = 0
     final_result = 0
     for word in words_list:
         if word in positive_comments_dictionary:
@@ -250,6 +221,8 @@ while True:
 
 
     neutral_result_file.write(str(round(final_result, 2)) + "\n")
+    if -0.1 < final_result < 0.1:
+        true_categorization += 1
     comments_counter += 1
 
 neutral_comments_precision = round((true_categorization / comments_counter) * 100, 2)
@@ -260,9 +233,11 @@ neutral_result_file.close()
 
 print("Total number of positive test tweets: " + str(total_positive_comments))
 print("Positive test tweets precision: " + str(positive_comments_precision) + " %")
+print("More than zero: " + str(more_than_zero_counter) + "    " + str(round(more_than_zero_counter / total_positive_comments, 2)))
 print("==============================================================================")
 print("Total number of negative test tweets: " + str(total_negative_comments))
 print("Negative test tweets precision: " + str(negative_comments_precision) + " %")
+print("Lower than zero: " + str(lower_than_zero_counter) + "    " + str(round(lower_than_zero_counter / total_negative_comments, 2)))
 print("==============================================================================")
 print("Total number of neutral test tweets: " + str(total_neutral_comments))
 print("Neutral test tweets precision: " + str(neutral_comments_precision) + " %")
