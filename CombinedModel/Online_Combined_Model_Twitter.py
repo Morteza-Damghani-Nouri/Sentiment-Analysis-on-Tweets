1e8e31bc27a0d0d0f38ff0e1f2b36c7efc52afcb
Morteza Damghani <mortezadamghani77@gmail.com>
2022-03-05 06:26:47 -0800

    Some changes added.

--- CombinedModel/Online_Combined_Model_Twitter.py
+++ CombinedModel/Online_Combined_Model_Twitter.py
@@ -20,10 +20,8 @@ def sentiment_detector(input_tweet):
         number_of_negative_words = total_number_of_negative_dictionary_words
         number_of_unique_words = unique_words_amount
         input_classifier = classifier
-
         lower_input_tweet = input_tweet.lower()
         me_predicted_label = input_classifier.classify(nltk_input_list_generator_online_version(lower_input_tweet))
-
         new_input_tweet = comment_smoother(lower_input_tweet)
         words_list = new_input_tweet.split(" ")
         final_result = 0
@@ -68,19 +66,15 @@ def sentiment_detector(input_tweet):
             final_label = 0
 
         if final_label == 1:
-            tweets_text_box.config(state=NORMAL)
             tweets_text_box.insert(END, input_tweet.rstrip("\n") + "\n", "positive")
         if final_label == 0:
-            tweets_text_box.config(state=NORMAL)
             tweets_text_box.insert(END, input_tweet.rstrip("\n") + "\n", "neutral")
         if final_label == -1:
-            tweets_text_box.config(state=NORMAL)
             tweets_text_box.insert(END, input_tweet.rstrip("\n") + "\n", "negative")
         tweets_text_box.insert(END, "================================================================================\n", "separator")
-        tweets_text_box.config(state=DISABLED)
-        T.delete("1.0", "end")
-    else:
-        T.delete("1.0", "end")
+
+
+
 
 
 # This function removes the foreground text in text box
@@ -149,7 +143,6 @@ tweets_frame_scrollbar = Scrollbar(tweets_frame)
 tweets_text_box = Text(tweets_frame, height=25, width=80, relief=RIDGE, borderwidth=5, yscrollcommand=tweets_frame_scrollbar.set, bg="grey", font="calibry 12")
 tweets_frame_scrollbar.pack(side=RIGHT, fill=Y)
 tweets_text_box.insert(INSERT, "Recent Analyzed Tweets:\n")
-tweets_text_box.config(state=DISABLED)
 tweets_text_box.tag_config("positive", foreground="green")
 tweets_text_box.tag_config("negative", foreground="red")
 tweets_text_box.tag_config("neutral", foreground="blue")
@@ -159,18 +152,31 @@ tweets_text_box.tag_config("separator", foreground="black")
 input_text_box_frame = tk.Frame(window)
 T_scrollbar = Scrollbar(input_text_box_frame)
 T = Text(input_text_box_frame, height=5, width=70, relief=RIDGE, borderwidth=5, yscrollcommand=T_scrollbar.set, bg="grey", font="calibry 14")
-T.insert(INSERT, "Type tweet here...")
-T.bind("<Button-1>", text_box_foreground_eraser)
 T_scrollbar.config(command=T.yview)
 T_scrollbar.pack(side=RIGHT, fill=Y)
+T.tag_config("trend_topics", foreground="red")
+T.tag_config("topics", foreground="orange")
 
 
 # Create button for next text
-b1 = Button(window, text ="Analyze Text", command=sentiment_detector, relief=RIDGE, borderwidth=3, font="calibri 12")
+b1 = Button(window, text ="Analyze Text", relief=RIDGE, borderwidth=3, font="calibri 12")
 
 # Create an Exit button
 b2 = Button(window, text ="Exit", command = window.destroy, relief=RIDGE, borderwidth=3, font="calibri 12")
 
+# Showing received data in GUI
+T.insert(INSERT, "Top 10 Trend Twitter Topics:\n", "trend_topics")
+for i in range(10):
+    T.insert(END, str(i + 1) + ")  " + trend_topics_names[i] + "\n", "topics")
+T.config(state=DISABLED)
+print("Top 50 trend topics are: ")
+for name in trend_topics_names:
+    print(name)
+print("==============================================================================")
+for tweet in received_tweets:
+    sentiment_detector(tweet)
+my_dataset.close()
+tweets_text_box.config(state=DISABLED)
 tweets_frame.pack(pady=(20, 20))
 tweets_text_box.pack()
 input_text_box_frame.pack(pady=(10, 20))
@@ -178,19 +184,6 @@ T.pack()
 b1.pack(pady=(0, 10))
 b2.pack()
 tk.mainloop()
-
-
-print("Top 50 trend topics are: ")
-for name in trend_topics_names:
-    print(name)
-print("==============================================================================")
-
-for tweet in received_tweets:
-    sentiment_detector(tweet)
-my_dataset.close()
-
-
-
 
 
 
