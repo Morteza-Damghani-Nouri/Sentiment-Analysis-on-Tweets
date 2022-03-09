3f2c201bc63740b770fe9f231b3bc5b244b32ae4
Morteza Damghani <mortezadamghani77@gmail.com>
2022-03-09 10:36:53 -0800

    Some progress is achieved.

--- CombinedModel/Combined_Offline_New.py
+++ CombinedModel/Combined_Offline_New.py
@@ -98,20 +98,6 @@ def nb_predictor(input_test_file_address, input_positive_dictionary, input_negat
     return output_results
 
 
-def precision_printer(input_results, input_desired_label, input_method_name):
-    counter = 0
-    for result in input_results:
-        if result == input_desired_label:
-            counter += 1
-    if input_desired_label == 1:
-        print(input_method_name + " positive precision: " + str(round((counter / len(input_results)), 2)) + " %")
-    if input_desired_label == 0:
-        print(input_method_name + " neutral precision: " + str(round((counter / len(input_results)), 2)) + " %")
-    if input_desired_label == -1:
-        print(input_method_name + " negative precision: " + str(round((counter / len(input_results)), 2)) + " %")
-    print("===========================================================================================================")
-
-
 # This function generates Maximum Entropy model results for the input test file
 def me_predictor(input_test_file_address, input_desired_label, input_classifier):
     output_results = []
@@ -140,8 +126,13 @@ def fcnn_predictor(input_test_x_file_address, input_fcnn_model):
     return output_results
 
 
-
-
+# This function calculates the precision of different models on different test data and initializes related variable
+def precision_calculator(input_results, input_desired_label):
+    counter = 0
+    for result in input_results:
+        if result == input_desired_label:
+            counter += 1
+    return round((counter / len(input_results)) * 100, 2)
 
 
 
@@ -159,123 +150,43 @@ number_of_unique_words = unique_words_counter(positive_tweets_dictionary, negati
 # Evaluating the positive test tweets
 positive_test_tweets_file_address = "E://MortezaDamghaniNouri//Computer Engineering//Semesters//9//Computer Engineering Final Project//Final Decision Files//Dataset//Test//positive_test.txt"
 nb_positive_test_results = nb_predictor(positive_test_tweets_file_address, positive_tweets_dictionary, negative_tweets_dictionary, total_number_of_positive_dictionary_words, total_number_of_negative_dictionary_words, number_of_unique_words)
-print("The length of nb_positive_test_results: " + str(len(nb_positive_test_results)))
-precision_printer(nb_positive_test_results, 1, "nb")
 me_positive_test_results = me_predictor(positive_test_tweets_file_address, 1, me_classifier)
-print("The length of me_positive_test_results: " + str(len(me_positive_test_results)))
-precision_printer(me_positive_test_results, 1, "me")
 fcnn_positive_test_tweets_file_address = "E://MortezaDamghaniNouri//MyCodes//Python Codes//Computer Engineering Final Project//FCNN//Test_X//positive_test_x_complete.txt"
 fcnn_positive_test_results = fcnn_predictor(fcnn_positive_test_tweets_file_address, fcnn_model)
-print("The length of fcnn_positive_test_results: " + str(len(fcnn_positive_test_results)))
-precision_printer(fcnn_positive_test_results, 1, "fcnn")
-
-
-
-
-
-
-
 
 # Evaluating the negative test tweets
-me_results = []
-nb_results = []
-final_negative_results = []
-negative_test_comments_file_address = "E://MortezaDamghaniNouri//Computer Engineering//Semesters//9//Computer Engineering Final Project//Final Decision Files//Dataset//Test//negative_test.txt"
-negative_test_comments_file = open(negative_test_comments_file_address, "rt", encoding="utf8")
-comments_counter = 0
-true_categorization = 0
-while True:
-    comment = negative_test_comments_file.readline().lower()
-    if comment == "":
-        break
-
-
-    comment = comment_smoother(comment)
-    words_list = comment.split(" ")
-    final_result = 0
-    for word in words_list:
-        if word in positive_comments_dictionary:
-            positive_numerator = positive_comments_dictionary[word] + 1
-        else:
-            positive_numerator = 1
-
-        if word in negative_comments_dictionary:
-            negative_numerator = negative_comments_dictionary[word] + 1
-        else:
-            negative_numerator = 1
-
-        final_result += math.log10((positive_numerator / (total_number_of_positive_dictionary_words + number_of_unique_words)) / (negative_numerator / (total_number_of_negative_dictionary_words + number_of_unique_words)))
-    if final_result < 0:
-        nb_results.append(-1)
-    if final_result > 0:
-        nb_results.append(1)
-    if final_result == 0:
-        nb_results.append(0)
-    comments_counter += 1
-
-
-# Generating the test list for negative test tweets
-test_list = nltk_input_list_generator(negative_test_comments_file_address, -1, [])
-
-# Evaluating the model for negative tweets by Maximum Entropy model
-for tweet_tuple in test_list:
-    tweet_word_dictionary = tweet_tuple[0]
-    main_label = tweet_tuple[1]
-    predicted_label = classifier.classify(tweet_word_dictionary)
-    me_results.append(predicted_label)
-
-negative_precision = round((final_evaluator(nb_results, me_results, final_negative_results, -1) / comments_counter) * 100, 2)
+negative_test_tweets_file_address = "E://MortezaDamghaniNouri//Computer Engineering//Semesters//9//Computer Engineering Final Project//Final Decision Files//Dataset//Test//negative_test.txt"
+nb_negative_test_results = nb_predictor(negative_test_tweets_file_address, positive_tweets_dictionary, negative_tweets_dictionary, total_number_of_positive_dictionary_words, total_number_of_negative_dictionary_words, number_of_unique_words)
+me_negative_test_results = me_predictor(negative_test_tweets_file_address, -1, me_classifier)
+fcnn_negative_test_tweets_file_address = "E://MortezaDamghaniNouri//MyCodes//Python Codes//Computer Engineering Final Project//FCNN//Test_X//negative_test_x_complete.txt"
+fcnn_negative_test_results = fcnn_predictor(fcnn_negative_test_tweets_file_address, fcnn_model)
 
 # Evaluating the neutral test tweets
-me_results = []
-nb_results = []
-final_neutral_results = []
-neutral_test_comments_file_address = "E://MortezaDamghaniNouri//Computer Engineering//Semesters//9//Computer Engineering Final Project//Final Decision Files//Dataset//Test//neutral_test.txt"
-neutral_test_comments_file = open(neutral_test_comments_file_address, "rt", encoding="utf8")
-comments_counter = 0
-true_categorization = 0
-while True:
-    comment = neutral_test_comments_file.readline().lower()
-    if comment == "":
-        break
-    comment = comment_smoother(comment)
-    words_list = comment.split(" ")
-    final_result = 0
-    for word in words_list:
-        if word in positive_comments_dictionary:
-            positive_numerator = positive_comments_dictionary[word] + 1
-        else:
-            positive_numerator = 1
+neutral_test_tweets_file_address = "E://MortezaDamghaniNouri//Computer Engineering//Semesters//9//Computer Engineering Final Project//Final Decision Files//Dataset//Test//neutral_test.txt"
+nb_neutral_test_results = nb_predictor(neutral_test_tweets_file_address, positive_tweets_dictionary, negative_tweets_dictionary, total_number_of_positive_dictionary_words, total_number_of_negative_dictionary_words, number_of_unique_words)
+me_neutral_test_results = me_predictor(neutral_test_tweets_file_address, 0, me_classifier)
+fcnn_neutral_test_tweets_file_address = "E://MortezaDamghaniNouri//MyCodes//Python Codes//Computer Engineering Final Project//FCNN//Test_X//neutral_test_x_complete.txt"
+fcnn_neutral_test_results = fcnn_predictor(fcnn_neutral_test_tweets_file_address, fcnn_model)
 
-        if word in negative_comments_dictionary:
-            negative_numerator = negative_comments_dictionary[word] + 1
-        else:
-            negative_numerator = 1
+NB_POSITIVE_PRECISION = precision_calculator(nb_positive_test_results, 1)
+NB_NEGATIVE_PRECISION = precision_calculator(nb_negative_test_results, -1)
+NB_NEUTRAL_PRECISION = precision_calculator(nb_neutral_test_results, 0)
+ME_POSITIVE_PRECISION = precision_calculator(me_positive_test_results, 1)
+ME_NEGATIVE_PRECISION = precision_calculator(me_negative_test_results, -1)
+ME_NEUTRAL_PRECISION = precision_calculator(me_neutral_test_results, 0)
+FCNN_POSITIVE_PRECISION = precision_calculator(fcnn_positive_test_results, 1)
+FCNN_NEGATIVE_PRECISION = precision_calculator(fcnn_negative_test_results, -1)
+FCNN_NEUTRAL_PRECISION = precision_calculator(fcnn_neutral_test_results, 0)
 
-        final_result += math.log10((positive_numerator / (total_number_of_positive_dictionary_words + number_of_unique_words)) / (negative_numerator / (total_number_of_negative_dictionary_words + number_of_unique_words)))
-    if final_result < 0:
-        nb_results.append(-1)
-    if final_result > 0:
-        nb_results.append(1)
-    if final_result == 0:
-        nb_results.append(0)
-    comments_counter += 1
+# Calculating combined model precision for positive test data
+final_evaluator(nb_positive_test_results, me_positive_test_results, fcnn_positive_test_results, 1)
 
+# Calculating combined model precision for negative test data
+final_evaluator(nb_negative_test_results, me_negative_test_results, fcnn_negative_test_results, -1)
 
-# Generating the test list for neutral test tweets
-test_list = nltk_input_list_generator(neutral_test_comments_file_address, 0, [])
+# Calculating combined model precision for neutral test data
+final_evaluator(nb_neutral_test_results, me_neutral_test_results, fcnn_neutral_test_results, 0)
 
-# Evaluating the model for neutral tweets by Maximum Entropy model
-for tweet_tuple in test_list:
-    tweet_word_dictionary = tweet_tuple[0]
-    main_label = tweet_tuple[1]
-    predicted_label = classifier.classify(tweet_word_dictionary)
-    me_results.append(predicted_label)
-neutral_precision = round((final_evaluator(nb_results, me_results, final_neutral_results, 0) / comments_counter) * 100, 2)
-
-print("The model precision for positive tweets: " + str(positive_precision) + " %")
-print("The model precision for negative tweets: " + str(negative_precision) + " %")
-print("The model precision for neutral tweets: " + str(neutral_precision) + " %")
 
 
 
